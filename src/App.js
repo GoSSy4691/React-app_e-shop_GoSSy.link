f132e548d6ac25e2d165e77cc74b9673f7e83a1c
GoSSy4691 <vitea1994@gmail.com>
2021-07-07 22:59:24 +0900

    feat: add some "lasers" which didn't work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import logo from './files/rotateIt.png';
 import './App.css';
+import laser from './files/laser.png';
+
+function makeDog(e) {
+  e.target.setAttribute( "src", laser);
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="cat" onClick={makeDog}/>
+        {/*if remove 'className="App-logo"' function makeDog work*/}
         <p>
           It goose TIME
           &nbsp;
           <button onClick={""}>
             Activate Lasers
+          </button>
+          <button
+              onClick={makeDog}
+          >
+            Test button
           </button>
         </p>
         <a

86b761f6a2683bcfe37a1bb04a2cb31100e914ec
GoSSy4691 <75677866+GoSSy4691@users.noreply.github.com>
2021-07-07 17:18:18 +0900

    refactor: change build-in files in basic react-app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './files/rotateIt.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          It goose TIME
+          &nbsp;
+          <button onClick={""}>
+            Activate Lasers
+          </button>
         </p>
         <a
           className="App-link"
@@ -15,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Start
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -11,7 +11,16 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// const name = 'Michael';
+// var nameUser = () => "Franklin";
+// const element = <h1>Здравствуй, {name}</h1>;
+// const element_2 = <h1>Здравствуй, {nameUser()}</h1>;
+//
+// ReactDOM.render(element, document.getElementById("root"));
+// ReactDOM.render(element_2, document.getElementById("root_2"));
+
+//Output
+// Здравствуй, Michael
+// Здравствуй, Franklin

1d2b74cd181bde9d0fd5486107bc00f6581ae048
GoSSy4691 <vitea1994@gmail.com>
2021-07-10 21:52:22 +0900

    feat: now "lasers" work, but rotation is broken
    moved @keyframes to up in App.css

--- src/App.js
+++ src/App.js
@@ -2,36 +2,32 @@ import logo from './files/rotateIt.png';
 import './App.css';
 import laser from './files/laser.png';
 
-function makeDog(e) {
-  e.target.setAttribute( "src", laser);
+function makeFire() {
+  let img = document.getElementById(13);
+  let fire = document.createElement('img');
+  fire.src = laser;
+  fire.className = "image2";
+  fire.alt = "lasers";
+  img.append(fire);
+  setTimeout(() => {
+    fire.remove()
+  }, 1000);
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="cat" onClick={makeDog}/>
-        {/*if remove 'className="App-logo"' function makeDog work*/}
-        <p>
+        <div className="parent" id={13}>
+          <img src={logo} className="image1" alt="goose"/>
+        </div>
+        <p className={"App-header2"}>
           It goose TIME
           &nbsp;
-          <button onClick={""}>
+          <button onClick={makeFire}>
             Activate Lasers
           </button>
-          <button
-              onClick={makeDog}
-          >
-            Test button
-          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Start
-        </a>
       </header>
     </div>
   );

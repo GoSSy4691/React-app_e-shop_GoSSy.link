2d9cc48d89eaa7ff5df77ffb8bc312c85960a775
GoSSy4691 <vitea1994@gmail.com>
2021-07-12 17:27:24 +0900

    feat: better text, border in toDoList
    add closeButton.png (don't work yet)

--- src/axios/toDoList.js
+++ src/axios/toDoList.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import render from "../render.js";
 import "./windowList.css"
+import closeButton from "../files/closeButton.png"
 
 let toDos = [];
 
 export function getToDos() {
   stateButton = false
-  axios.get('https://jsonplaceholder.typicode.com/todos/')
+  axios.get("https://jsonplaceholder.typicode.com/todos/")
     .then((res) => {
       toDos = res.data;
       render()
@@ -17,8 +18,9 @@ export function getToDos() {
 }
 
 export function getList() {
-  let list = toDos.map(p => <li key={Math.random()}>{p.title}</li>);
+  let list = toDos.map(p => <li key={Math.random()} className={"list"}>{p.title}</li>);
   return <div className={"form-popup"}>
+      <img src={closeButton} className={"closeButton"} alt={"closeButtonInToDoList"}/>
     <form className={"form-container"}>{list}</form>
   </div>
 }
@@ -28,7 +30,7 @@ let stateButton = true;
 export function listButton() {
   if (stateButton) {
     return (
-      <button onClick={getToDos} className={'open-button'}> Get list </button>
+      <button onClick={getToDos} className={"open-button"}> Get list </button>
     );
   } else {
     return (getList());

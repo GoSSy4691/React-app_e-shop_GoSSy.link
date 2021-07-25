ddd2f4c4d89f92f35c730c185758a5339babfc8d
GoSSy4691 <vitea1994@gmail.com>
2021-07-25 22:16:46 +0900

    feat: menu element hover effect
    change some height in css menu

--- src/mainPage/containers/menu/menu.js
+++ src/mainPage/containers/menu/menu.js
@@ -3,17 +3,18 @@ import emptyImg from '../../../files/img/noItem.png';
 
 function Menu(props) {
   return <div className={s.showRoom}>
-    {props.menuItems[0].items.menu.map(p => <div className={s.foodElement} key={p.id}>
-      <div className={s.item}>
-        <img src={emptyImg} className={s.foodImg} alt={'logo'}/>
-        <div className={s.namePrice}>
-        <li>{p.name}</li>
-        <li>{p.cost}</li>
+    {props.menuItems[0].items.menu.map(p =>
+      <div className={s.foodElement} key={p.id}>
+        <div className={s.item}>
+          <img src={emptyImg} className={s.foodImg} alt={'logo'}/>
+          <div className={s.namePrice}>
+            <li>{p.name}</li>
+            <li>{p.cost}</li>
+          </div>
+          <div className={s.description}>
+            <li>{p.description}</li>
+          </div>
         </div>
-        <div className={s.description}>
-          <li>{p.description}</li>
-        </div>
-      </div>
     </div>)}
   </div>;
 }

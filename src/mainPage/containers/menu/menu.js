1cfe471910b934ec2ad56e044bbae2bceee57eca
GoSSy4691 <vitea1994@gmail.com>
2021-07-26 18:42:50 +0900

    feat: add shopCart in menu (doesn't work yet)

--- src/mainPage/containers/menu/menu.js
+++ src/mainPage/containers/menu/menu.js
@@ -1,22 +1,34 @@
 import s from './menu.module.css';
 import emptyImg from '../../../files/img/noItem.png';
+import shopCart from '../../../files/img/shopCart.png';
+
+function ShopCart() {
+  return (
+    <div className={s.topBar}>
+      <img src={shopCart} className={s.shopIco} alt={'shopCart'} onClick={() => console.log("go to shop")}/>
+      <div className={s.shopIcoCount}>0</div>
+    </div>
+  );
+}
 
 function Menu(props) {
-  return <div className={s.showRoom}>
-    {props.menuItems[0].items.menu.map(p =>
-      <div className={s.foodElement} key={p.id}>
-        <div className={s.item}>
-          <img src={emptyImg} className={s.foodImg} alt={'logo'}/>
-          <div className={s.namePrice}>
-            <li>{p.name}</li>
-            <li>{p.cost}</li>
+  return (
+    <div className={s.showRoom}>
+      <ShopCart/>
+      {props.menuItems[0].items.menu.map(p =>
+        <div className={s.foodElement} key={p.id}>
+          <div className={s.item}>
+            <img src={emptyImg} className={s.foodImg} alt={'logo'}/>
+            <div className={s.namePrice}>
+              <li>{p.name}</li>
+              <li>{p.cost}</li>
+            </div>
+            <div className={s.description}>
+              <li>{p.description}</li>
+            </div>
           </div>
-          <div className={s.description}>
-            <li>{p.description}</li>
-          </div>
-        </div>
-    </div>)}
-  </div>;
+        </div>)}
+    </div>);
 }
 
 export default Menu;

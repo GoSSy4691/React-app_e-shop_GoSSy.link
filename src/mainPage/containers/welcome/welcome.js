6d4953c609c4ea10825ed84eafda905a0a2337af
GoSSy4691 <vitea1994@gmail.com>
2021-07-28 23:57:43 +0900

    feat: rework welcome page

--- src/mainPage/containers/welcome/welcome.js
+++ src/mainPage/containers/welcome/welcome.js
@@ -1,9 +1,18 @@
 import s from './welcome.module.css'
+import welcomeImg from '../../../files/img/welcomeImg.png'
 
 function Welcome() {
   return (
     <div className={s.divContainer}>
-      <div className={s.containerText}>It's goose TIME</div>
+      <img src={welcomeImg} className={s.welcomeImg} key={Math.random()} alt={'welcomeImg'}/>
+      <div className={s.description}>
+        Hi &#128400; this site created for learning React's capabilities.
+        <br/>
+        I just beginner at developing and in the future want know more about front development.
+        <br/>
+        Please bear with me and my code.
+      </div>
+      <div className={s.ps}>It's goose TIME</div>
     </div>
   );
 }

8294ee5c777bacd11a725287709e3ca4f66357ca
GoSSy4691 <vitea1994@gmail.com>
2021-10-21 15:37:46 +0900

    fix: API params for category

--- src/files/API/api.js
+++ src/files/API/api.js
@@ -10,10 +10,7 @@ export default class API {
   }
 
   static getCategory(id_point) {
-    let localToken =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NjAsImlhdCI6MTYzNDc1NTE4NSwiZXhwIjoxNjM1NjE5MTg1fQ.ecNPT9XnwDwXParo2ElflxCmu_r5n-nLLwtk0Nr6jHs";
     return axios.get("https://zloi.space/restaurant/api/menu/category", {
-      headers: { Authorization: `Bearer ${localToken}` },
       params: { id_point: id_point },
     });
   }

e7090ddadb3a45090ad914f6a9dbf448630d2ee2
GoSSy4691 <vitea1994@gmail.com>
2021-11-03 14:15:59 +0900

    redesign: dashboard points

--- src/packages/menu/dashboard/AdminBar.jsx
+++ src/packages/menu/dashboard/AdminBar.jsx
@@ -5,10 +5,7 @@ import PointsDialog from "./PointsDialog.jsx";
 
 import menuOpenSVG from "../../../files/img/adminBar/00_menuOpen.svg";
 import userSVG from "../../../files/img/adminBar/01_user.svg";
-import shopSVG from "../../../files/img/adminBar/02_shop.svg";
 import menuSVG from "../../../files/img/adminBar/03_menu.svg";
-import imagesSVG from "../../../files/img/adminBar/04_images.svg";
-import foodsSVG from "../../../files/img/adminBar/05_foods.svg";
 import cartSVG from "../../../files/img/adminBar/06_cart.svg";
 import statsSVG from "../../../files/img/adminBar/07_stats.svg";
 import dialogSVG from "../../../files/img/adminBar/08_dialog.svg";
@@ -40,23 +37,11 @@ export default function AdminBar() {
           {isBarOpen && <p>Users</p>}
         </button>
         <button onClick={() => setPointsShow(!isPointsShow)}>
-          <img alt={"shop"} src={shopSVG} />
-          {isBarOpen && <p>Points</p>}
-        </button>
-        <button>
           <img alt={"menu"} src={menuSVG} />
           {isBarOpen && <p>Menu</p>}
         </button>
         <button>
-          <img alt={"images"} src={imagesSVG} />
-          {isBarOpen && <p>Images</p>}
-        </button>
-        <button>
-          <img alt={"foods"} src={foodsSVG} />
-          {isBarOpen && <p>Foods</p>}
-        </button>
-        <button>
-          <img alt={"cart"} src={cartSVG} />
+          <img alt={"orders"} src={cartSVG} />
           {isBarOpen && <p>Orders</p>}
         </button>
         <button>

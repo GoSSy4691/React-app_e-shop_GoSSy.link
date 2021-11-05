1c497ec4ef7ad4df431dd0c287cb36e8555c695d
GoSSy4691 <vitea1994@gmail.com>
2021-11-05 18:21:40 +0900

    refactor: cursor pointer on local server

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,26 @@ import { useDispatch } from "react-redux";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Cookies from "universal-cookie";
 import styled from "styled-components";
-import getCursor from "./files/getCursor.js";
 import Header from "./packages/header/Header.jsx";
 import About from "./packages/about/About.jsx";
 import ShopsMenu from "./packages/menu/ShopsMenu.jsx";
 import OrderView from "./packages/menu/OrderView.jsx";
 import ErrorPopup from "./packages/popup/ErrorPopup.jsx";
 
-import { useState } from "react";
+import cursorAuto from "./files/img/cursor/cursorAuto.png";
+import cursorPointer from "./files/img/cursor/cursorPointer.png";
 
 const CustomCursorDiv = styled.div`
-  cursor: url(${(props) => (props.cursor.isLoaded ? props.cursor.auto : null)}),
-    auto;
+  cursor: url(${cursorAuto}), auto;
   * button {
-    cursor: url(${(props) =>
-        props.cursor.isLoaded ? props.cursor.pointer : null}),
-      auto;
+    cursor: url(${cursorPointer}), auto;
   }
   * a {
-    cursor: url(${(props) =>
-        props.cursor.isLoaded ? props.cursor.pointer : null}),
-      auto;
+    cursor: url(${cursorPointer}), auto;
   }
 `;
 
 export default function App() {
-  const [cursor, setCursor] = useState({ isLoaded: false });
   const dispatch = useDispatch();
   const cookies = new Cookies();
 
@@ -54,29 +48,8 @@ export default function App() {
     }
   }
 
-  if (!cursor.isLoaded) {
-    getCursor()
-      .then((res) => {
-        setCursor({
-          isLoaded: true,
-          auto: res.cursorAuto,
-          pointer: res.cursorPointer,
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-        setCursor({
-          isLoaded: "Error",
-        });
-      });
-  }
-
   return (
-    <CustomCursorDiv
-      className="app"
-      onScroll={(e) => scrollObserver(e.target)}
-      cursor={cursor}
-    >
+    <CustomCursorDiv className="app" onScroll={(e) => scrollObserver(e.target)}>
       <Header />
       <Switch>
         <Route exact path="/" component={ShopsMenu} />

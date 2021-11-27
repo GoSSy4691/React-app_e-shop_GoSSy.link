54e444987302b2e2d40d11b96a0bc103379abe6c
GoSSy4691 <vitea1994@gmail.com>
2021-11-27 19:27:36 +0900

    fix: loading loop in dashboard's orders dialog

--- src/packages/dashboard/OrdersDialog.jsx
+++ src/packages/dashboard/OrdersDialog.jsx
@@ -1,6 +1,6 @@
 import patternDashboard from "./CSS/patternDashboard.module.css";
 import patternMenu from "../patternMenu.module.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Cookies from "universal-cookie";
 import useDetectClickOut from "../../files/useDetectClickOut.js";
@@ -11,22 +11,25 @@ import updateSVG from "../../files/img/update-arrows.svg";
 export default function PointsDialog() {
   const orders = useSelector((state) => state.admin.orders);
   const scrollInOrders = useSelector((state) => state.admin.scrollInOrders);
+  const [emptyDialog, setEmptyDialog] = useState("Loading...");
   const dispatch = useDispatch();
   const cookies = new Cookies();
   const refBox = useDetectClickOut(() => dispatch({ type: "SET_BAR_SHOW" }));
   const scrollAbleDiv = useRef(null);
 
   function loadingOrders() {
+    if (emptyDialog !== "Loading...") setEmptyDialog("Loading...");
     zloiAPI
       .getOrders(cookies.get("Token"))
       .then((res) => {
+        if (res.data.data.length === 0) setEmptyDialog("Empty");
         dispatch({ type: "LOAD_ALL_ORDERS", payload: res.data.data });
       })
       .catch((error) => {
         console.error(error);
+        setEmptyDialog("Error");
         dispatch({ type: "ERROR_MESSAGE", payload: "Loading orders error" });
       });
-    return <p>Loading...</p>;
   }
 
   function formattedDate(el_date) {
@@ -43,6 +46,12 @@ export default function PointsDialog() {
     }
   }, [scrollInOrders]);
 
+  //auto loading orders
+  useEffect(() => {
+    loadingOrders();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [orders.length]);
+
   return (
     <div className={patternMenu.darkenBackground}>
       <div className={patternDashboard.showBox} ref={refBox}>
@@ -54,13 +63,16 @@ export default function PointsDialog() {
         </button>
         <button
           className={patternMenu.updateButton}
-          onClick={() => dispatch({ type: "CLEAN_ORDERS_LIST" })}
+          onClick={() => {
+            dispatch({ type: "CLEAN_ORDERS_LIST" });
+            loadingOrders();
+          }}
         >
           <img alt={"update"} src={updateSVG} draggable="false" />
         </button>
         <div className={patternDashboard.usersTitle}>Orders:</div>
         {orders.length === 0 ? (
-          loadingOrders()
+          <p>{emptyDialog}</p>
         ) : (
           <>
             <li className={patternDashboard.line} key={"title"}>
